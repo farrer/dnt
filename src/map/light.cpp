0d6dac7cc25c19c1434fd5584c0a731f2f347ef0
Farrer <farrer@dnteam.org>
2016-12-07 21:49:04 -0200

    Changing lights on indoor maps, depending of character position (missing files).

---
+++ src/map/light.cpp
@@ -0,0 +1,197 @@
+/* 
+  DNT: a satirical post-apocalyptical RPG.
+  Copyright (C) DNTeam <dnt@dnteam.org>
+ 
+  This file is part of DNT.
+ 
+  DNT is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  DNT is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with DNT.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "light.h"
+
+#include "../core/game.h"
+
+#include <OGRE/OgreMath.h>
+
+using namespace DNT;
+
+/**************************************************************************
+ *                                Constructor                             *
+ **************************************************************************/
+LightInfo::LightInfo(Ogre::Light::LightTypes type)
+{
+   this->type = type;
+}
+
+/**************************************************************************
+ *                                 Destructor                             *
+ **************************************************************************/
+LightInfo::~LightInfo()
+{
+}
+
+/**************************************************************************
+ *                               setPosition                              *
+ **************************************************************************/
+void LightInfo::setPosition(Ogre::Vector3 pos)
+{
+   this->position = pos;
+}
+
+/**************************************************************************
+ *                               setDirection                             *
+ **************************************************************************/
+void LightInfo::setDirection(Ogre::Vector3 dir)
+{
+   this->direction = dir;
+}
+
+/**************************************************************************
+ *                               setDiffuse                               *
+ **************************************************************************/
+void LightInfo::setDiffuse(float r, float g, float b)
+{
+   this->diffuse = Ogre::ColourValue(r, g, b);
+}
+
+/**************************************************************************
+ *                               setSpecular                              *
+ **************************************************************************/
+void LightInfo::setSpecular(float r, float g, float b)
+{
+   this->specular = Ogre::ColourValue(r, g, b);
+}
+
+/**************************************************************************
+ *                           setSpolightRange                             *
+ **************************************************************************/
+void LightInfo::setSpotlightRange(Ogre::Degree outerAngle)
+{
+   this->outerAngle = outerAngle;
+}
+
+/**************************************************************************
+ *                                setArea                                 *
+ **************************************************************************/
+void LightInfo::setArea(int x1, int y1, int x2, int y2)
+{
+   area = Farso::Rect(x1, y1, x2, y2);
+}
+
+/**************************************************************************
+ *                                isInner                                 *
+ **************************************************************************/
+bool LightInfo::isInner(int pX, int pZ)
+{
+   return area.isInner(pX, pZ);
+}
+
+/**************************************************************************
+ *                               setLight                                 *
+ **************************************************************************/
+void LightInfo::setLight(Ogre::Light* light)
+{
+   /* Define common light elements */
+   light->setType(type);
+   light->setPosition(position);
+   light->setDiffuseColour(diffuse);
+   light->setSpecularColour(specular);
+
+   if(type != Ogre::Light::LT_POINT)
+   {
+      /* Use direction */
+      light->setDirection(direction);
+   }
+   if(type == Ogre::Light::LT_SPOTLIGHT)
+   {
+      /* Set spotlight outer angle */
+      light->setSpotlightRange(Ogre::Degree(0), outerAngle);
+   }
+}
+
+/**************************************************************************
+ *                              MapLights                                 *
+ **************************************************************************/
+MapLights::MapLights()
+{
+   light = Game::getSceneManager()->createLight();
+   lastX = -1;
+   lastZ = -1;
+   curLight = NULL;
+}
+
+/**************************************************************************
+ *                             ~MapLights                                 *
+ **************************************************************************/
+MapLights::~MapLights()
+{
+   if(light)
+   {
+      Game::getSceneManager()->destroyLight(light);
+   }
+}
+
+/**************************************************************************
+ *                         createLightInfo                                *
+ **************************************************************************/
+LightInfo* MapLights::createLightInfo(Ogre::Light::LightTypes type)
+{
+   LightInfo* info = new LightInfo(type);
+   lights.insertAtEnd(info);
+
+   return info;
+}
+
+/**************************************************************************
+ *                             setActiveLight                             *
+ **************************************************************************/
+void MapLights::setActiveLight(float pX, float pZ)
+{
+   int iX = Ogre::Math::Floor(pX);
+   int iZ = Ogre::Math::Floor(pZ);
+
+   if((iX != lastX) || (iZ != lastZ))
+   {
+      lastX = iX;
+      lastZ = iZ;
+      
+      /* Note: assuming that full map is always covered by at least one area. */
+      LightInfo* info = static_cast<LightInfo*>(lights.getFirst());
+      for(int i = 0; i < lights.getTotal(); i++)
+      {
+         if(info->isInner(iX, iZ))
+         {
+            /* Found light, let's see if changed */
+            if(curLight != info)
+            {
+               curLight = info;
+               info->setLight(light);
+            }
+            return;
+         }
+         info = static_cast<LightInfo*>(info->getNext());
+      }
+      
+      /* No light found... must disable current? 
+       * For now just using the first one. */
+      info = static_cast<LightInfo*>(lights.getFirst());
+      if(curLight != info)
+      {
+         curLight = info;
+         info->setLight(light);
+      }
+   }
+}
+
+
---
+++ src/map/light.h
@@ -0,0 +1,120 @@
+/* 
+  DNT: a satirical post-apocalyptical RPG.
+  Copyright (C) DNTeam <dnt@dnteam.org>
+ 
+  This file is part of DNT.
+ 
+  DNT is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  DNT is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with DNT.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _dnt_map_light_h
+#define _dnt_map_light_h
+
+#include "dntconfig.h"
+
+#include <OGRE/OgreLight.h>
+
+#include <kobold/list.h>
+#include <kobold/kstring.h>
+
+#include <farso/rect.h>
+
+namespace DNT
+{
+   
+   /*! Keep light information in a map */
+   class LightInfo : public Kobold::ListElement
+   {
+      public:
+         /*! Constructor */
+         LightInfo(Ogre::Light::LightTypes type);
+         /*! Destructor */
+         ~LightInfo();
+
+         /*! Set light position
+          * \param pos new light position */
+         void setPosition(Ogre::Vector3 pos);
+
+         /*! Set light direction
+          * \param dir new light direction */
+         void setDirection(Ogre::Vector3 dir);
+
+         /*! Set diffuse light color */
+         void setDiffuse(float r, float g, float b);
+
+         /*! Set specular light color */
+         void setSpecular(float r, float g, float b);
+
+         /*! Set outer angle of the spotlight range */
+         void setSpotlightRange(Ogre::Degree outerAngle);
+
+         /*! Set affected area */
+         void setArea(int x1, int y1, int x2, int y2);
+
+         /*! \return if point is inner affected area */
+         bool isInner(int pX, int pZ);
+
+         /*! Set an Ogre light to be as defined by this LightInfo */
+         void setLight(Ogre::Light* light);
+
+      private:
+         Ogre::ColourValue diffuse;   /**< Diffuse color */
+         Ogre::ColourValue specular;  /**< Specular color */
+
+         Ogre::Vector3 position; /**< Light position */
+         Ogre::Vector3 direction; /**< Light direction */
+
+         Ogre::Degree outerAngle; /**< Spolight outerAngle */
+
+         Ogre::Light::LightTypes type; /**< Type of the light used */
+
+         Farso::Rect area;  /**< Area where the light will act */
+
+   };
+
+   /*! Class that keeps information about all lights on a map, selecting
+    * those active based on current character position */
+   class MapLights : public Kobold::List
+   {
+      public:
+         /*! Constructor */
+         MapLights();
+         /*! Destructor */
+         ~MapLights();
+
+         /*! Create a LightInfo for a Map light definition.
+          * \param type light type
+          * \return pointer to the created LightInfo */
+         LightInfo* createLightInfo(Ogre::Light::LightTypes type);
+
+         /*! Set active light for position on map.
+          * \param pX X coordinate
+          * \param pZ Z coordinate
+          * \note if more than one light is defined for the position (ie: 
+          *       overlapped areas), the first one is used */
+         void setActiveLight(float pX, float pZ);
+
+      private:
+         Ogre::Light* light;  /**< Current active light (Ogre) */
+         LightInfo* curLight; /**< Current active light */
+         Kobold::List lights; /**< Map Lights */
+
+         int lastX;  /**< Last X coordinate of the last check */
+         int lastZ;  /**< Last Z coordinate of the last chack */
+   };
+
+}
+
+#endif
+
