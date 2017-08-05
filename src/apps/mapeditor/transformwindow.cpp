feaca66b98ed29750b88337c508e094a37e44bb0
Farrer <farrer@dnteam.org>
2017-08-05 11:33:42 -0300

    Must use the 'now' set functions.

--- src/apps/mapeditor/transformwindow.cpp
+++ src/apps/mapeditor/transformwindow.cpp
@@ -176,7 +176,7 @@ bool TransformWindow::checkEvents(PositionEditor* positionEditor)
          /* Check positions */
          if(event.getWidget() == position[0])
          {
-            thing->getModel()->setPosition(
+            thing->getModel()->setPositionNow(
                   parse(position[0], thing->getModel()->getPosition().x),
                   thing->getModel()->getPosition().y,
                   thing->getModel()->getPosition().z);
@@ -184,7 +184,7 @@ bool TransformWindow::checkEvents(PositionEditor* positionEditor)
          }
          else if(event.getWidget() == position[1])
          {
-            thing->getModel()->setPosition(
+            thing->getModel()->setPositionNow(
                   thing->getModel()->getPosition().x,
                   parse(position[1], thing->getModel()->getPosition().y),
                   thing->getModel()->getPosition().z);
@@ -192,7 +192,7 @@ bool TransformWindow::checkEvents(PositionEditor* positionEditor)
          }
          else if(event.getWidget() == position[2])
          {
-            thing->getModel()->setPosition(
+            thing->getModel()->setPositionNow(
                   thing->getModel()->getPosition().x,
                   thing->getModel()->getPosition().y,
                   parse(position[2], thing->getModel()->getPosition().z));
@@ -202,40 +202,40 @@ bool TransformWindow::checkEvents(PositionEditor* positionEditor)
          else if(event.getWidget() == rotation[0])
          {
             thing->getModel()->clearOrientation();
-            thing->getModel()->setOrientation(
+            thing->getModel()->setOrientationNow(
                   parse(rotation[0], ori.x), ori.y, ori.z);
             positionEditor->updateAxisPosition();
          }
          else if(event.getWidget() == rotation[1])
          {
             thing->getModel()->clearOrientation();
-            thing->getModel()->setOrientation(
+            thing->getModel()->setOrientationNow(
                   ori.x, parse(rotation[1], ori.y), ori.z);
          }
          else if(event.getWidget() == rotation[2])
          {
             thing->getModel()->clearOrientation();
-            thing->getModel()->setOrientation(
+            thing->getModel()->setOrientationNow(
                   ori.x, ori.y, parse(rotation[2], ori.z));
          }
          /* Check scale */
          else if(event.getWidget() == scale[0])
          {
-            thing->getModel()->setScale(
+            thing->getModel()->setScaleNow(
                   parse(scale[0], thing->getModel()->getScale().x),
                   thing->getModel()->getScale().y,
                   thing->getModel()->getScale().z);
          }
          else if(event.getWidget() == scale[1])
          {
-            thing->getModel()->setScale(
+            thing->getModel()->setScaleNow(
                   thing->getModel()->getScale().x,
                   parse(scale[1], thing->getModel()->getScale().y),
                   thing->getModel()->getScale().z);
          }
          else if(event.getWidget() == scale[2])
          {
-            thing->getModel()->setScale(
+            thing->getModel()->setScaleNow(
                   thing->getModel()->getScale().x,
                   thing->getModel()->getScale().y,
                   parse(scale[2], thing->getModel()->getScale().z));
